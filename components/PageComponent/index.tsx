0a1649575fd8fdc61da09c0902e4599a69470bc8
Breno Lins <brenolins100@gmail.com>
2022-04-12 23:50:11 -0300

    Finalizando as telas login, cadastro, sobre, sugestões

--- components/PageComponent/index.tsx
+++ components/PageComponent/index.tsx
@@ -8,16 +8,16 @@ interface IGenericPage {
     rightElement?: React.ReactNode;
 }
 
-const GenericPage = ({children, title, rightElement}: IGenericPage) => {
+const GenericPage = ({ children, title, rightElement }: IGenericPage) => {
     const { mode } = useAppThemeContext();
     return (
-            <Box bgcolor={mode == 'light' ? Pallete.light.fundo2.main : Pallete.dark.fundo2.main} p={5} m={{ xs: 2, sm: 5, lg: 7, xl: 10 }} boxShadow="0 3px 5px #00000040" borderRadius={5}>
-                <Stack direction="row" alignItems='center'>
-                    <Typography fontSize={30} fontWeight='bold' mr={2}>{title}</Typography>
-                    {rightElement}
-                </Stack>
-                {children}
-            </Box>
+        <Box bgcolor={mode == 'light' ? Pallete.light.fundo2.main : Pallete.dark.fundo2.main} p={5} m={{ xs: 2, sm: 5, lg: 7, xl: 10 }} boxShadow="0 3px 5px #00000040" borderRadius={2}>
+            <Stack direction="row" alignItems='center'>
+                <Typography fontSize={30} fontWeight='bold' mr={2}>{title}</Typography>
+                {rightElement}
+            </Stack>
+            {children}
+        </Box>
     )
 }
 

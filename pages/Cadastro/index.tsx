5f8f5a4f4cb876a438bb86bb07b2da487396b1ed
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2022-04-19 01:32:32 -0300

    Ajustes logi e cadastro

--- pages/Cadastro/index.tsx
+++ pages/Cadastro/index.tsx
@@ -1,5 +1,5 @@
 
-import { Grid, Button, Stack } from '@mui/material'
+import { Button, Stack, Box } from '@mui/material'
 
 import { Mail, Phone, Password, Badge, PersonAddAlt } from '@mui/icons-material'
 
@@ -8,32 +8,20 @@ import PageComponent from '../../components/PageComponent'
 
 function Cadastro () {
   return (
-
-    <PageComponent title="Cadastro">
-      <Grid container columnSpacing={{ xs: '10%', lg: '5%' }} rowSpacing={3} pt={1}>
-        <Grid item xs={12} sm={6} lg={4}>
+    <Box width='100%' display='flex' justifyContent='center'>
+      <PageComponent title="Cadastro" sx={{ width: '30%', minWidth: '360px' }}>
+        <Stack direction='column' spacing={3}>
           <InputComponent label='Nome' icon={<Badge />} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={4}>
           <InputComponent label='Telefone' icon={<Phone />} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={4}>
           <InputComponent label='E-mail' icon={<Mail />} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={4}>
           <InputComponent label='Senha' icon={<Password />} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={4}>
           <InputComponent label='Confirmar senha' icon={<Password />} />
-        </Grid>
-      </Grid>
-      <Stack direction='row' mt={4}>
-        <Button variant='contained' startIcon={<PersonAddAlt />}>
-          Cadastrar
-        </Button>
-      </Stack>
-    </PageComponent>
-
+          <Button variant='contained' startIcon={<PersonAddAlt />}>
+            Cadastrar
+          </Button>
+        </Stack>
+      </PageComponent>
+    </Box>
   )
 }
 
--- pages/Login/index.tsx
+++ pages/Login/index.tsx
@@ -1,29 +1,24 @@
 
-import { Grid, Button, Stack } from '@mui/material'
-import { Mail, Password } from '@mui/icons-material'
-import LoginIcon from '@mui/icons-material/Login'
+import { Button, Stack } from '@mui/material'
+import { Mail, Password, Login as LoginIcon } from '@mui/icons-material'
 
 import InputComponent from '../../components/InputComponent'
 import PageComponent from '../../components/PageComponent'
+import { Box } from '@mui/system'
 
 function Login () {
   return (
-
-    <PageComponent title="Logar">
-      <Grid container columnSpacing={{ xs: '10%', lg: '5%' }} rowSpacing={3} pt={1}>
-        <Grid item xs={12} sm={6} lg={4}>
+    <Box width='100%' display='flex' justifyContent='center'>
+    <PageComponent title="Logar" sx={{ width: '30%', minWidth: '360px' }}>
+      <Stack direction='column' spacing={3}>
           <InputComponent label='E-mail' icon={<Mail />} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={4}>
           <InputComponent label='Senha' icon={<Password />} />
-        </Grid>
-      </Grid>
-      <Stack direction='row' mt={4}>
         <Button variant='contained' startIcon={<LoginIcon />}>
           Login
         </Button>
       </Stack>
     </PageComponent>
+    </Box>
   )
 }
 
